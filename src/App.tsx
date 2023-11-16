7bb3d7ce65e79ffdc8d84d3d125c5a81b9499c1c
peipeix2 <peipei.hsueh@outlook.com>
2023-11-16 20:28:38 +0800

    Set up firestore database

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
     return (
         <>
-            <div className="text-red bg-sky-700 px-4 py-2 hover:bg-sky-800">
+            <div className="text-red border-solid border-gray-400 bg-sky-700 px-4 py-2 hover:bg-sky-800">
                 <h1>Hello World</h1>
             </div>
         </>
