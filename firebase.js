4654b3aeb63eea11f3d3e747c3bb93880a18eb2c
peipeix2 <peipei.hsueh@outlook.com>
2023-11-16 16:33:55 +0800

    Initialize project: Set up Typescript, Tailwind and Firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyAYbo5vGylXFm0y940WHiG03r8JS7fAo0k',
+    authDomain: 'filter-14ea1.firebaseapp.com',
+    projectId: 'filter-14ea1',
+    storageBucket: 'filter-14ea1.appspot.com',
+    messagingSenderId: '355785761965',
+    appId: '1:355785761965:web:e180103aca7552280d5224',
+    measurementId: 'G-VDBZCCYD69',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="text-red bg-sky-700 px-4 py-2 hover:bg-sky-800">
+                <h1>Hello World</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
