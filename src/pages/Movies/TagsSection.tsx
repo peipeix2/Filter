1b5077fbade4a43d03a73454a72600b8a5831019
peipeix2 <peipei.hsueh@outlook.com>
2023-11-28 21:18:04 +0800

    Delete redundant codes

--- src/pages/Movies/TagsSection.tsx
+++ src/pages/Movies/TagsSection.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { collection, query, getDocs, where, collectionGroup } from 'firebase/firestore'
+import { query, getDocs, where, collectionGroup } from 'firebase/firestore'
 import { db } from '../../../firebase'
 
 interface MoviesCommentsForIdState {
