141a83e71e1e7cf9e0b6e88a3576e5d80d519975
peipeix2 <peipei.hsueh@outlook.com>
2023-12-01 11:21:26 +0800

    Add key to movie cards at homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -105,136 +105,135 @@ const Home = () => {
     }
 
     return (
-        <>
-            <Navbar
-                position="static"
-                className="mx-auto my-5 flex w-4/5 items-center justify-around bg-slate-400"
-            >
-                <NavbarContent>
-                    <span>篩選電影</span>
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button variant="bordered">類型</Button>
-                        </DropdownTrigger>
-                        <DropdownMenu>
-                            <DropdownItem>喜劇</DropdownItem>
-                            <DropdownItem>劇情</DropdownItem>
-                            <DropdownItem>動作</DropdownItem>
-                            <DropdownItem>冒險</DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
+      <>
+        <Navbar
+          position="static"
+          className="mx-auto my-5 flex w-4/5 items-center justify-around bg-slate-400"
+        >
+          <NavbarContent>
+            <span>篩選電影</span>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="bordered">類型</Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem>喜劇</DropdownItem>
+                <DropdownItem>劇情</DropdownItem>
+                <DropdownItem>動作</DropdownItem>
+                <DropdownItem>冒險</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
 
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button variant="bordered">評分</Button>
-                        </DropdownTrigger>
-                        <DropdownMenu>
-                            <DropdownItem>5分</DropdownItem>
-                            <DropdownItem>4分</DropdownItem>
-                            <DropdownItem>3分</DropdownItem>
-                            <DropdownItem>2分</DropdownItem>
-                            <DropdownItem>1分</DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </NavbarContent>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="bordered">評分</Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem>5分</DropdownItem>
+                <DropdownItem>4分</DropdownItem>
+                <DropdownItem>3分</DropdownItem>
+                <DropdownItem>2分</DropdownItem>
+                <DropdownItem>1分</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarContent>
 
-                <div className="flex items-center">
-                    <Input
-                        classNames={{
-                            base: 'max-w-full sm:max-w-[10rem] h-10',
-                            mainWrapper: 'h-full',
-                            input: 'text-small',
-                            inputWrapper:
-                                'h-full font-normal text-default-500 bg-slate-800 dark:bg-default-500/20',
-                        }}
-                        placeholder="搜尋電影"
-                        size="sm"
-                        startContent={<CiSearch size={18} />}
-                        type="search"
-                    />
-                </div>
-            </Navbar>
+          <div className="flex items-center">
+            <Input
+              classNames={{
+                base: 'max-w-full sm:max-w-[10rem] h-10',
+                mainWrapper: 'h-full',
+                input: 'text-small',
+                inputWrapper:
+                  'h-full font-normal text-default-500 bg-slate-800 dark:bg-default-500/20',
+              }}
+              placeholder="搜尋電影"
+              size="sm"
+              startContent={<CiSearch size={18} />}
+              type="search"
+            />
+          </div>
+        </Navbar>
 
-            <div className="mx-auto my-10 w-4/5">
-                <h3 className="font-semibold">熱門電影</h3>
-                <div className="my-5 flex gap-2">
-                    {moviesFromAPI.map((movie) => {
-                        return (
-                            <Link to={`/movies/${movie.id}`}>
-                                <Card className="w-23% py-4" key={movie.id}>
-                                    <CardBody>
-                                        <Image
-                                            alt="film-poster"
-                                            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                                        />
-                                    </CardBody>
-                                    <CardHeader className="flex-col items-center">
-                                        <p className="text-center text-xs">
-                                            {movie.title}
-                                        </p>
-                                        <small className="text-center text-xs">
-                                            {movie.original_title}
-                                        </small>
-                                        {moviesRating.map((item) => {
-                                            if (item.id === movie.id) {
-                                                return (
-                                                    <Star
-                                                        rating={item.rating}
-                                                        count={
-                                                            item.ratings_count
-                                                        }
-                                                    />
-                                                )
-                                            }
-                                        })}
-                                    </CardHeader>
-                                </Card>
-                            </Link>
-                        )
-                    })}
-                </div>
+        <div className="mx-auto my-10 w-4/5">
+          <div className='flex items-center justify-between'>
+            <h3 className="font-semibold">熱門電影</h3>
+            <span>More</span>
+          </div>
+
+          <div className="my-5 flex gap-2">
+            {moviesFromAPI.map((movie, index) => {
+              return (
+                <Link to={`/movies/${movie.id}`} key={index}>
+                  <Card className="w-23% py-4">
+                    <CardBody>
+                      <Image
+                        alt="film-poster"
+                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      />
+                    </CardBody>
+                    <CardHeader className="flex-col items-center">
+                      <p className="text-center text-xs">{movie.title}</p>
+                      <small className="text-center text-xs">
+                        {movie.original_title}
+                      </small>
+                      {moviesRating.map((item) => {
+                        if (item.id === movie.id) {
+                          return (
+                            <Star
+                              rating={item.rating}
+                              count={item.ratings_count}
+                            />
+                          )
+                        }
+                      })}
+                    </CardHeader>
+                  </Card>
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className="mx-auto my-10 w-4/5">
+          <div className='flex items-center justify-between'>
+            <h3 className="font-semibold">正在上映</h3>
+            <span>More</span>
             </div>
-
-            <div className="mx-auto my-10 w-4/5">
-                <h3 className="font-semibold">正在上映</h3>
-                <div className="my-5 flex gap-2">
-                    {nowPlaying.map((movie) => {
-                        return (
-                            <Link to={`/movies/${movie.id}`}>
-                                <Card className="w-23% py-4" key={movie.id}>
-                                    <CardBody>
-                                        <Image
-                                            alt="film-poster"
-                                            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                                        />
-                                    </CardBody>
-                                    <CardHeader className="flex-col items-center">
-                                        <p className="text-center">
-                                            {movie.title}
-                                        </p>
-                                        <small className="text-center">
-                                            {movie.original_title}
-                                        </small>
-                                        {moviesRating.map((item) => {
-                                            if (item.id === movie.id) {
-                                                return (
-                                                    <Star
-                                                        rating={item.rating}
-                                                        count={
-                                                            item.ratings_count
-                                                        }
-                                                    />
-                                                )
-                                            }
-                                        })}
-                                    </CardHeader>
-                                </Card>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
-        </>
+          <div className="my-5 flex gap-2">
+            {nowPlaying.map((movie, index) => {
+              return (
+                <Link to={`/movies/${movie.id}`} key={index}>
+                  <Card className="w-23% py-4">
+                    <CardBody>
+                      <Image
+                        alt="film-poster"
+                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      />
+                    </CardBody>
+                    <CardHeader className="flex-col items-center">
+                      <p className="text-center">{movie.title}</p>
+                      <small className="text-center">
+                        {movie.original_title}
+                      </small>
+                      {moviesRating.map((item) => {
+                        if (item.id === movie.id) {
+                          return (
+                            <Star
+                              rating={item.rating}
+                              count={item.ratings_count}
+                            />
+                          )
+                        }
+                      })}
+                    </CardHeader>
+                  </Card>
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+      </>
     )
 }
 
