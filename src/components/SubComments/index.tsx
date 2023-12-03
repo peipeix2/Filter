217c0a71cae2f12cfd3b0feebfccf36f800b5ea1
peipeix2 <peipei.hsueh@outlook.com>
2023-12-03 19:44:18 +0800

    Adjust comment page UI

--- src/components/SubComments/index.tsx
+++ src/components/SubComments/index.tsx
@@ -1,11 +1,19 @@
-import { useState, useEffect } from "react"
-import { db } from "../../../firebase"
-import { collection, onSnapshot, setDoc, addDoc, doc, serverTimestamp, deleteDoc } from "firebase/firestore"
+import { useState, useEffect } from 'react'
+import { db } from '../../../firebase'
+import {
+  collection,
+  onSnapshot,
+  setDoc,
+  addDoc,
+  doc,
+  serverTimestamp,
+  deleteDoc,
+} from 'firebase/firestore'
 import { Textarea, Button, Divider } from '@nextui-org/react'
-import useUserStore from "../../store/userStore"
+import useUserStore from '../../store/userStore'
 
 interface SubCommentsState {
-  commentId: string,
+  commentId: string
   userId: string
 }
 
@@ -17,29 +25,50 @@ const SubComments = (Props: SubCommentsState) => {
   const user = useUserStore((state) => state.user)
 
   useEffect(() => {
-    const commentRef = collection(db, "USERS", Props.userId, "COMMENTS", Props.commentId, "SUBCOMMENTS")
-    const unsubscribe = onSnapshot(commentRef, (querySnapshot) => {
-      const subCommentsData:any = []
-      querySnapshot.forEach((doc) => {
-        subCommentsData.push({...doc.data(), id: doc.id})
-      })
-      const subCommentsDataSorted = subCommentsData.sort((a:any,b:any) => a.created_at - b.created_at)
-      setSubComments(subCommentsDataSorted)
-    })
-
-    return () => {
-      unsubscribe()
-    }
+    // const commentRef = collection(
+    //   db,
+    //   'USERS',
+    //   Props.userId,
+    //   'COMMENTS',
+    //   Props.commentId,
+    //   'SUBCOMMENTS'
+    // )
+    // const unsubscribe = onSnapshot(commentRef, (querySnapshot) => {
+    //   const subCommentsData: any = []
+    //   querySnapshot.forEach((doc) => {
+    //     subCommentsData.push({ ...doc.data(), id: doc.id })
+    //   })
+    //   const subCommentsDataSorted = subCommentsData.sort(
+    //     (a: any, b: any) => a.created_at - b.created_at
+    //   )
+    //   setSubComments(subCommentsDataSorted)
+    // })
+    // return () => {
+    //   unsubscribe()
+    // }
   }, [])
 
-  const handlePostComment = async() => {
+  const handlePostComment = async () => {
     if (!text || text?.trim().length === 0) {
       alert('評論不得為空')
       return
     }
 
-    const subCommentRef = collection(db, "USERS", Props.userId, "COMMENTS", Props.commentId, "SUBCOMMENTS")
-    const commentRef = doc(db, 'USERS', Props.userId, 'COMMENTS', Props.commentId)
+    const subCommentRef = collection(
+      db,
+      'USERS',
+      Props.userId,
+      'COMMENTS',
+      Props.commentId,
+      'SUBCOMMENTS'
+    )
+    const commentRef = doc(
+      db,
+      'USERS',
+      Props.userId,
+      'COMMENTS',
+      Props.commentId
+    )
 
     const postSubCommentData = {
       userId: user.userId,
@@ -47,34 +76,57 @@ const SubComments = (Props: SubCommentsState) => {
       subcomment: text,
       created_at: serverTimestamp(),
       updated_at: serverTimestamp(),
-      comment_id: Props.commentId
+      comment_id: Props.commentId,
     }
 
     try {
       await addDoc(subCommentRef, postSubCommentData)
-      await setDoc(commentRef, {comments_count: subComments.length + 1}, { merge: true })
+      await setDoc(
+        commentRef,
+        { comments_count: subComments.length + 1 },
+        { merge: true }
+      )
       console.log('Add subComment successfully.')
       setText('')
     } catch (err) {
       console.log('Error', err)
     }
-    
   }
 
-  const handleEditComment = async(subCommentId: string, subCommentBody: string) => {
+  const handleEditComment = async (
+    subCommentId: string,
+    subCommentBody: string
+  ) => {
     if (editingComments.includes(subCommentId)) {
-      setEditingComments((prev:any) => prev.filter((id:string) => id !== subCommentId))
+      setEditingComments((prev: any) =>
+        prev.filter((id: string) => id !== subCommentId)
+      )
     } else {
       setEditingComments((prev: any) => [...prev, subCommentId])
     }
-    setEditTextMap((prev:any) => ({ ...prev, [subCommentId]: subCommentBody }))
+    setEditTextMap((prev: any) => ({ ...prev, [subCommentId]: subCommentBody }))
   }
 
-  const handleUpdateComment = async(subCommentId: string) => {
-    const subCommentRef = doc(db, "USERS", Props.userId, "COMMENTS", Props.commentId, "SUBCOMMENTS", subCommentId)
+  const handleUpdateComment = async (subCommentId: string) => {
+    const subCommentRef = doc(
+      db,
+      'USERS',
+      Props.userId,
+      'COMMENTS',
+      Props.commentId,
+      'SUBCOMMENTS',
+      subCommentId
+    )
 
     try {
-      await setDoc(subCommentRef, {subcomment: editTextMap[subCommentId], updated_at: serverTimestamp()}, { merge:true })
+      await setDoc(
+        subCommentRef,
+        {
+          subcomment: editTextMap[subCommentId],
+          updated_at: serverTimestamp(),
+        },
+        { merge: true }
+      )
       setEditingComments((prev: any) =>
         prev.filter((id: string) => id !== subCommentId)
       )
@@ -83,28 +135,48 @@ const SubComments = (Props: SubCommentsState) => {
     }
   }
 
-  const handleDeleteComment = async (subCommentId:string) => {
-    const subCommentRef = doc(db, "USERS", Props.userId, "COMMENTS", Props.commentId, "SUBCOMMENTS", subCommentId)
-    const commentRef = doc(db, 'USERS', Props.userId, 'COMMENTS', Props.commentId)
+  const handleDeleteComment = async (subCommentId: string) => {
+    const subCommentRef = doc(
+      db,
+      'USERS',
+      Props.userId,
+      'COMMENTS',
+      Props.commentId,
+      'SUBCOMMENTS',
+      subCommentId
+    )
+    const commentRef = doc(
+      db,
+      'USERS',
+      Props.userId,
+      'COMMENTS',
+      Props.commentId
+    )
 
     try {
       await deleteDoc(subCommentRef)
-      await setDoc(commentRef, {comments_count: subComments.length - 1}, { merge: true })
+      await setDoc(
+        commentRef,
+        { comments_count: subComments.length - 1 },
+        { merge: true }
+      )
     } catch (err) {
       console.log('Error', err)
     }
   }
 
-  if(!subComments) return
+  if (!subComments) return
 
   return (
     <div className="w-full">
-      {subComments.map((subComment: any, index:number) => {
+      {subComments.map((subComment: any, index: number) => {
         return (
           <div className="subComment-card my-5" key={index}>
-            <div className="header flex gap-2">
+            <div className="header flex items-center gap-2">
               <span className="font-bold">{subComment.username}</span>
-              <span>{subComment.created_at?.toDate().toDateString()}</span>
+              <span className="text-sm text-slate-400">
+                {subComment.created_at?.toDate().toDateString()}
+              </span>
             </div>
 
             {editingComments.includes(subComment.id) ? (
@@ -119,7 +191,11 @@ const SubComments = (Props: SubCommentsState) => {
                   }
                 />
                 <div className="flex w-full justify-end">
-                  <Button className="mt-2" onClick={() => handleUpdateComment(subComment.id)}>
+                  <Button
+                    className="mt-2"
+                    size="sm"
+                    onClick={() => handleUpdateComment(subComment.id)}
+                  >
                     更新
                   </Button>
                 </div>
@@ -130,8 +206,9 @@ const SubComments = (Props: SubCommentsState) => {
               </div>
             )}
             {user.userId === subComment.userId && (
-              <div className="mt-3 text-right">
+              <div className="my-3 flex justify-end gap-2">
                 <Button
+                  size="sm"
                   onClick={() =>
                     handleEditComment(subComment.id, subComment.subcomment)
                   }
@@ -140,7 +217,11 @@ const SubComments = (Props: SubCommentsState) => {
                     ? '取消編輯'
                     : '編輯'}
                 </Button>
-                <Button onClick={() => handleDeleteComment(subComment.id)}>
+                <Button
+                  size="sm"
+                  color="danger"
+                  onClick={() => handleDeleteComment(subComment.id)}
+                >
                   刪除
                 </Button>
               </div>
