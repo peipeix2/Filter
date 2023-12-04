523190b4acfac46421e68580fa9963b1926e7edc
peipeix2 <peipei.hsueh@outlook.com>
2023-12-04 16:56:36 +0800

    Add site logo to header and footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,12 @@
 function Footer() {
   return (
     <div className="footer-container mt-[150px] h-[120px] w-full px-[128px] text-sm font-semibold text-[#222222]">
-      <h1>Site Logo</h1>
-      <div className='flex justify-between w-full font-thin mt-10'>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/filter-14ea1.appspot.com/o/Filter-logos_transparent_cut.png?alt=media&token=d3119f34-30f8-4afe-8e1c-6e74367acf7c"
+        alt="site-logo"
+        className="h-auto w-[200px]"
+      />
+      <div className="mt-5 flex w-full justify-between font-thin">
         <ul className="legal term flex gap-3">
           <li>Cookie Policy</li>
           <li>Legal Terms</li>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import { CiSearch } from 'react-icons/ci'
 import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    Input,
-    DropdownItem,
-    DropdownTrigger,
-    Dropdown,
-    DropdownMenu,
-    Avatar,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  Input,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
 } from '@nextui-org/react'
 import SignUp from '../SignUp'
 import SignIn from '../SignIn'
@@ -17,73 +17,75 @@ import useUserStore from '../../store/userStore'
 import SubNavbar from '../../pages/Home/SubNavbar'
 
 function Header() {
-    const { user, isLogin } = useUserStore()
+  const { user, isLogin } = useUserStore()
 
-    if (!user) return
+  if (!user) return
 
-    return (
-      <Navbar
-        className="items-center bg-white px-32"
-        maxWidth="full"
-        height="90px"
-        position='static'
-      >
-        <NavbarBrand>
-          <a href="/">SiteLogo</a>
-        </NavbarBrand>
+  return (
+    <Navbar
+      className="items-center bg-white px-32"
+      maxWidth="full"
+      height="90px"
+      position="static"
+    >
+      <NavbarBrand>
+        <a href="/">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/filter-14ea1.appspot.com/o/Filter-logos_transparent_cut.png?alt=media&token=d3119f34-30f8-4afe-8e1c-6e74367acf7c"
+            alt="site-logo"
+            className="h-auto w-[300px]"
+          />
+        </a>
+      </NavbarBrand>
 
-        <NavbarContent className='flex items-center w-2/3 gap-5' justify='end'>
-          <SubNavbar />
-          {!isLogin && (
-            <>
-              <SignUp />
-              <SignIn />
-            </>
-          )}
-          {isLogin && (
-            <>
-              <Dropdown placement="bottom-end">
-                <DropdownTrigger>
-                  <Avatar
-                    isBordered
-                    as="button"
-                    className="transition-transform"
-                    color="default"
-                    name={user.username ? user.username : 'Guest'}
-                    size="sm"
-                    src={user.avatar}
-                  />
-                </DropdownTrigger>
-                <DropdownMenu aria-label="Profile Actions" variant="flat">
-                  <DropdownItem key="profile" className="h-14 gap-2">
-                    <p className="font-semibold">
-                      Signed in as {user.username}
-                    </p>
-                    <p className="font-semibold">{user.email}</p>
-                  </DropdownItem>
-                  <DropdownItem key="profile" href={`/profile/${user.userId}`}>
-                    個人頁面
-                  </DropdownItem>
-                  <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                  <DropdownItem key="analytics">Analytics</DropdownItem>
-                  <DropdownItem key="system">System</DropdownItem>
-                  <DropdownItem key="configurations">
-                    Configurations
-                  </DropdownItem>
-                  <DropdownItem key="help_and_feedback">
-                    Help & Feedback
-                  </DropdownItem>
-                  <DropdownItem key="logout" color="danger">
-                    Log Out
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <Logout />
-            </>
-          )}
-        </NavbarContent>
-      </Navbar>
-    )
+      <NavbarContent className="flex w-2/3 items-center gap-5" justify="end">
+        <SubNavbar />
+        {!isLogin && (
+          <>
+            <SignUp />
+            <SignIn />
+          </>
+        )}
+        {isLogin && (
+          <>
+            <Dropdown placement="bottom-end">
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as="button"
+                  className="transition-transform"
+                  color="default"
+                  name={user.username ? user.username : 'Guest'}
+                  size="sm"
+                  src={user.avatar}
+                />
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                  <p className="font-semibold">Signed in as {user.username}</p>
+                  <p className="font-semibold">{user.email}</p>
+                </DropdownItem>
+                <DropdownItem key="profile" href={`/profile/${user.userId}`}>
+                  個人頁面
+                </DropdownItem>
+                <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                <DropdownItem key="analytics">Analytics</DropdownItem>
+                <DropdownItem key="system">System</DropdownItem>
+                <DropdownItem key="configurations">Configurations</DropdownItem>
+                <DropdownItem key="help_and_feedback">
+                  Help & Feedback
+                </DropdownItem>
+                <DropdownItem key="logout" color="danger">
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <Logout />
+          </>
+        )}
+      </NavbarContent>
+    </Navbar>
+  )
 }
 
 export default Header
