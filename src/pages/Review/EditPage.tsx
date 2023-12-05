59bb33f46ddad3f6db11f6825a11896cb2cc9696
peipeix2 <peipei.hsueh@outlook.com>
2023-12-05 16:24:16 +0800

    Customize style for heading, orderlist, unorderlist and blockquote in text editor

--- src/pages/Review/EditPage.tsx
+++ src/pages/Review/EditPage.tsx
@@ -1,7 +1,12 @@
 import StarterKit from '@tiptap/starter-kit'
 import { useEditor, EditorContent } from '@tiptap/react'
+import Heading from '@tiptap/extension-heading'
 import Underline from '@tiptap/extension-underline'
 import Image from '@tiptap/extension-image'
+import Blockquote from '@tiptap/extension-blockquote'
+import BulletList from '@tiptap/extension-bullet-list'
+import OrderedList from '@tiptap/extension-ordered-list'
+import { mergeAttributes } from '@tiptap/react'
 import { useState, useEffect, useRef } from 'react'
 import { storage } from '../../../firebase'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
@@ -31,8 +36,7 @@ import { db } from '../../../firebase'
 import { useNavigate, useParams } from 'react-router-dom'
 import TagsInput from '../../components/TagsInput'
 import useUserStore from '../../store/userStore'
-
-const extensions = [StarterKit, Underline, Image]
+import { AiOutlineOrderedList, AiOutlineUnorderedList } from 'react-icons/ai'
 
 const EditPage = () => {
   const [moviesData, setMoviesData] = useState<any>([])
@@ -91,7 +95,49 @@ const EditPage = () => {
         return text.toUpperCase()
       },
     },
-    extensions,
+    extensions: [
+      StarterKit,
+      Underline,
+      Image,
+      Heading.configure({ levels: [1, 2, 3] })
+        .extend({
+          levels: [1, 2, 3],
+          renderHTML({ node, HTMLAttributes }) {
+            const level = this.options.levels.includes(node.attrs.level)
+              ? node.attrs.level
+              : this.options.levels[0]
+            const classes: { [index: number]: string } = {
+              1: 'text-2xl font-extrabold',
+              2: 'text-xl font-extrabold',
+              3: 'text-lg font-bold',
+            }
+            return [
+              `h${level}`,
+              mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+                class: `${classes[level]}`,
+              }),
+              0,
+            ]
+          },
+        })
+        .configure({ levels: [1, 2, 3] }),
+      Blockquote.configure({
+        HTMLAttributes: {
+          class:
+            'p-4 my-4 italic font-medium leading-relaxed text-gray-900 dark:text-white border-l-4 border-gray-300',
+        },
+      }),
+      OrderedList.configure({
+        HTMLAttributes: {
+          class: 'p-4 list-decimal',
+        },
+      }),
+      BulletList.configure({
+        HTMLAttributes: {
+          class: 'p-4 list-disc',
+        },
+      }),
+    ],
     content,
     onUpdate: ({ editor }) => {
       const html = editor.getHTML()
@@ -291,6 +337,18 @@ const EditPage = () => {
           }
         >
           <FaQuoteLeft />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          <AiOutlineOrderedList className="text-xl font-bold" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          <AiOutlineUnorderedList className="text-xl font-bold" />
         </button>
         <button
           onClick={() => editor.chain().focus().undo().run()}
