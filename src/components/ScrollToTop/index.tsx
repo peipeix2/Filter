e6d341053f57095ccf43fee8452adfa10b09f7b3
peipeix2 <peipei.hsueh@outlook.com>
2023-12-12 09:39:50 +0800

    Exclude tabs for movie details page from scroll-to-top effect

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -4,9 +4,17 @@ import { useLocation } from 'react-router-dom'
 const ScrollToTop = () => {
   const { pathname } = useLocation()
   const isProfilePage = pathname.includes('profile')
+  const isMoviesCastPage = pathname.includes('cast')
+  const isMoviesCrewPage = pathname.includes('crew')
+  const isMoviesDetailsPage = pathname.includes('details')
 
   useEffect(() => {
-    if (!isProfilePage) {
+    if (
+      !isProfilePage &&
+      !isMoviesCastPage &&
+      !isMoviesCrewPage &&
+      !isMoviesDetailsPage
+    ) {
       window.scrollTo(0, 0)
     }
   }, [pathname])
