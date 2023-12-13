833dbfdb83ad31f7533ebdc5b953b193b686e225
peipeix2 <peipei.hsueh@outlook.com>
2023-12-13 20:05:48 +0800

    Adjust tag padding

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -11,7 +11,7 @@ const Tag = (Props: TagState) => {
   return (
     <Link to={`/tag?keyword=${Props.tag}`}>
       <Chip
-        className="bg-[#94a3ab] p-1 text-xs text-slate-100"
+        className="bg-[#94a3ab] px-2 py-1 text-xs text-slate-100"
         key={Props.index}
         size="sm"
         startContent={<FaTag size={12} color="#f2f5f9" />}
